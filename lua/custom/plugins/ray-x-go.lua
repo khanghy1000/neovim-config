e68e52fd5dfdb63de9f5b36412c86a5ab875d0df
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-10-07 23:39:08 +0700

    feat: make ray-x/go.nvim run in float window

--- lua/custom/plugins/ray-x-go.lua
+++ lua/custom/plugins/ray-x-go.lua
@@ -5,9 +5,11 @@ return {
     'neovim/nvim-lspconfig',
     'nvim-treesitter/nvim-treesitter',
   },
-  config = function()
-    require('go').setup()
-  end,
+  opts = {
+    run_in_floaterm = true,
+    trouble = true,
+    luasnip = true,
+  },
   event = { 'CmdlineEnter' },
   ft = { 'go', 'gomod' },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
