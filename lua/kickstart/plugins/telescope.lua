5315484b9ab43f7ab9d25e091df614a4b349476d
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-01-25 09:32:45 +0700

    feat: change buffer fuzzy search keymap

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -159,13 +159,13 @@ return {
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>sb', function()
+      vim.keymap.set('n', '<leader>sc', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
           previewer = false,
         })
-      end, { desc = 'Fuzzily [S]earch in current [B]uffer' })
+      end, { desc = 'Fuzzily [S]earch in [C]urrent buffer' })
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
