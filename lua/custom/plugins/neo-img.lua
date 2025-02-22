196ed3ec58e56433ff02f4f5b0dd4df266218802
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-02-22 20:54:16 +0700

    chore

--- lua/custom/plugins/neo-img.lua
+++ lua/custom/plugins/neo-img.lua
@@ -1,11 +1,9 @@
 return {
   'skardyy/neo-img',
+  build = function()
+    require('neo-img').install()
+  end,
   config = function()
-    require('neo-img').setup {
-      size = {
-        oil = 1400,
-        main = 1400,
-      },
-    }
+    require('neo-img').setup {}
   end,
 }
