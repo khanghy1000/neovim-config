7edd6f73705676f68abf623c9e9db0aa35f4b9d9
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-04-22 00:07:02 +0700

    Enable snippets for blink

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -21,12 +21,19 @@ return {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              local extends = {
+                ['javascript'] = { 'jsdoc' },
+              }
+
+              for k, v in pairs(extends) do
+                require('luasnip').filetype_extend(k, v)
+              end
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
         opts = {},
       },
