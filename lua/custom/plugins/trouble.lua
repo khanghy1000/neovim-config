7db77ffbfa7ef9061e41eb3d5b2a29d5ff13898a
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-04-23 09:22:38 +0700

    Add Trouble keybind to snacks.picker

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -60,4 +60,24 @@ return {
       desc = '[L]ocation List',
     },
   },
+  specs = {
+    'folke/snacks.nvim',
+    opts = function(_, opts)
+      return vim.tbl_deep_extend('force', opts or {}, {
+        picker = {
+          actions = require('trouble.sources.snacks').actions,
+          win = {
+            input = {
+              keys = {
+                ['<c-t>'] = {
+                  'trouble_open',
+                  mode = { 'n', 'i' },
+                },
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
