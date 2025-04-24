3721dbd064a63fcfe1f25898dd5cba11b6996d39
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-04-24 08:24:23 +0700

    Disable copilot NES

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -12,14 +12,14 @@ return {
       },
     },
   },
-  {
-    'copilotlsp-nvim/copilot-lsp',
-    init = function()
-      vim.g.copilot_nes_debounce = 500
-      vim.lsp.enable 'copilot'
-      vim.keymap.set('n', '<tab>', function()
-        require('copilot-lsp.nes').apply_pending_nes()
-      end)
-    end,
-  },
+  -- {
+  --   'copilotlsp-nvim/copilot-lsp',
+  --   init = function()
+  --     vim.g.copilot_nes_debounce = 500
+  --     vim.lsp.enable 'copilot'
+  --     vim.keymap.set('n', '<tab>', function()
+  --       require('copilot-lsp.nes').apply_pending_nes()
+  --     end)
+  --   end,
+  -- },
 }
