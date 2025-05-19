3bf2427f8e56f137c5d6b2d2acd4d801c5926eb8
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-05-19 20:28:09 +0700

    Update nvim-treesitter-context config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,12 +7,12 @@ return {
   {
     'nvim-treesitter/nvim-treesitter-context',
     config = function()
-      vim.cmd [[hi TreesitterContextBottom gui=underline guisp=Grey]]
-      vim.cmd [[hi TreesitterContextLineNumberBottom gui=underline guisp=Grey]]
+      vim.api.nvim_set_hl(0, 'TreesitterContextBottom', { sp = '#89ACF3', underline = true })
+      vim.api.nvim_set_hl(0, 'TreesitterContextLineNumberBottom', { sp = '#89ACF3', underline = true })
 
-      vim.keymap.set('n', '[p', function()
+      vim.keymap.set('n', '[s', function()
         require('treesitter-context').go_to_context(vim.v.count1)
-      end, { silent = true, desc = 'Jump to [P]arent context' })
+      end, { silent = true, desc = 'Jump to [S]ticky scroll context' })
 
       require('treesitter-context').setup {
         multiline_threshold = 1,
